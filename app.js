5b38b74fee28924bac385d6a8d60bb3d69cc6e3e
HPRobbins <haileyrobbins202@gmail.com>
2023-03-31 17:15:26 -0400

    Delete Final 2

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const http=require('http');
-const server=http.createServer().listen(8080);
-const process=require('./lib/process.js');
+const http = require('http');
+const post = require('./controller/post');
+const server = http.createServer().listen(8080);
+//const process=require('./lib/process.js');
+const url = require('url');
+const fs = require('fs');
 
 server.on('request',async(req,res)=>{
-	process(req,res);
+	let myfile='file.json';
+	if(!fs.existsSync(`./data/${myfile}`)) {
+		fs.writeFileSync('./data/file.json','');
+	}
+
+	console.log(req.method);
+	//Parse the components of the URL
+	let url_components= url.parse(req.url,true);
+	console.log(url_components);
+	console.log(url_components.pathname.split('/'));
+	let path = url_components.pathname.split('/');
+	let stringPath = path[1];
+	if(req.method === "POST" && url_components.pathname === '/api') {
+		await post.savePostBody(req, res);
+	}
+	if(req.method === "DELETE" && stringPath === "api" ){
+		const delete = require('./controller/delete');
+		delete.getfunction(req,res,path[2]);
+	}
+	else {   
+		  res.statusCode = 404;   
+		  res.end('Page not found');  
+	}
 });
-
-
---
+++ controller/Delete.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const deleteReq = async function(req,res) {
+          const filePath = './data/file.json';   
+          
+        if (!fs.existsSync(filePath)) {      
+             res.statusCode = 404;     
+               res.end('File not found');      
+              return;   
+              }       
+                   res.statusCode = 405;     
+                  res.end('Deleting file is not enabled');    
+                   return;     
+            fs.unlinkSync(filePath);   
+
+            res.statusCode = 200;   
+              res.end('File deleted successfully');  
+
+            server.listen(3000, () => {  
+                console.log('Server started on port 3000');
+        })
+    };
